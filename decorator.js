ca9b8b2dc8eef67f0e6bdf9533804a29094197f1
anish.m.prasad <anish.m.prasad@gmail.com>
2019-03-20 21:44:50 +0530

    code refactor

--- decorator.js
+++ decorator.js
@@ -3,19 +3,20 @@ import LazyLoad from './index';
 
 const getDisplayName = WrappedComponent => WrappedComponent.displayName || WrappedComponent.name || 'Component';
 
-export default (options = {}) => function lazyload(WrappedComponent) {
-  return class LazyLoadDecorated extends Component {
-    constructor() {
-      super();
-      this.displayName = `LazyLoad${getDisplayName(WrappedComponent)}`;
-    }
+export default (options = {}) =>
+	function lazyload(WrappedComponent) {
+		return class LazyLoadDecorated extends Component {
+			constructor() {
+				super();
+				this.displayName = `LazyLoad${getDisplayName(WrappedComponent)}`;
+			}
 
-    render() {
-      return (
-        <LazyLoad {...options}>
-          <WrappedComponent {...this.props} />
-        </LazyLoad>
-      );
-    }
-  };
-};
+			render() {
+				return (
+					<LazyLoad {...options}>
+						<WrappedComponent {...this.props} />
+					</LazyLoad>
+				);
+			}
+		};
+	};
--- lazyload/utils/scrollParent.js
+++ lazyload/utils/scrollParent.js
@@ -2,37 +2,37 @@
  * @fileOverview Find scroll parent
  */
 
-export default (node) => {
-  if (!node) {
-    return document.documentElement;
-  }
+export default node => {
+	if (!node) {
+		return document.documentElement;
+	}
 
-  const excludeStaticParent = node.style.position === 'absolute';
-  const overflowRegex = /(scroll|auto)/;
-  let parent = node;
+	const excludeStaticParent = node.style.position === 'absolute';
+	const overflowRegex = /(scroll|auto)/;
+	let parent = node;
 
-  while (parent) {
-    if (!parent.parentNode) {
-      return node.ownerDocument || document.documentElement;
-    }
+	while (parent) {
+		if (!parent.parentNode) {
+			return node.ownerDocument || document.documentElement;
+		}
 
-    const style = window.getComputedStyle(parent);
-    const position = style.position;
-    const overflow = style.overflow;
-    const overflowX = style['overflow-x'];
-    const overflowY = style['overflow-y'];
+		const style = window.getComputedStyle(parent);
+		const position = style.position;
+		const overflow = style.overflow;
+		const overflowX = style['overflow-x'];
+		const overflowY = style['overflow-y'];
 
-    if (position === 'static' && excludeStaticParent) {
-      parent = parent.parentNode;
-      continue;
-    }
+		if (position === 'static' && excludeStaticParent) {
+			parent = parent.parentNode;
+			continue;
+		}
 
-    if (overflowRegex.test(overflow) && overflowRegex.test(overflowX) && overflowRegex.test(overflowY)) {
-      return parent;
-    }
+		if (overflowRegex.test(overflow) && overflowRegex.test(overflowX) && overflowRegex.test(overflowY)) {
+			return parent;
+		}
 
-    parent = parent.parentNode;
-  }
+		parent = parent.parentNode;
+	}
 
-  return node.ownerDocument || node.documentElement || document.documentElement;
+	return node.ownerDocument || node.documentElement || document.documentElement;
 };
